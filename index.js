cac4c21a364c5aeac42bb82e781525a97705a891
aldeander-santos@hotmail.com <aldeandersantos@hotmail.com>
2023-07-04 21:49:06 -0300

    :tada: Iniciando o projeto

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+
+router.get('/', function(req,res){
+
+    res.sendFile(path.join(__dirname+'/index.html'));
+
+})
+router.get('/sobre', function(req,res){
+
+    res.sendFile(path.join(__dirname+'/sobre.html'));
+
+})
+
+
+app.use('/', router);
+
+app.listen(process.env.port || 3000);
+
+console.log("Servidor rodando!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const res = require('express/lib/response');
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+
+const server = http.createServer((req,res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Olá mundo');
+});
+
+server.listen(port,hostname, () => {
+    console.log("servidor rodando!");
+})*/
